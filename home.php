32728864c398a668b6f089fc6bd3f24334a483d9
unknown <mm117@CHEK-0210-05.SGF.EDUBEAR.NET>
2015-01-27 18:43:19 -0600

    Mitch messes everything up

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <title>ACM</title>
 <link rel="type/css" type="css" href="acm.css" />
 <p>Matt is a collaborator</p>
-<p>Mitch is a swag nerd baller</p>
+<p>Mitch is a swag nerd baller who is using vim to type.</p>
 </head>
 
 <body>
